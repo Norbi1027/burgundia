51a2b3e22378b013f70560fcad78ea8ec0060dbe
sandor.norbert <sandor.norbert@BURG-1E05-20>
2023-02-06 09:27:06 +0100

    Add project files.

---
+++ animal/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animal
+{
+    internal class Program
+    {
+        enum Animal {cat = 2,wolf = 5,tiger = 10,dog = 3 };
+
+        static void Main(string[] args)
+        {
+            Animal a = Animal.wolf;
+            Animal b = Animal.tiger;
+
+            int c = 0;
+            string s = " ";
+
+            int? x = null;
+            
+
+            if (a != b)
+            {
+                Console.WriteLine("Az egér nem macska");
+            }
+            Console.WriteLine("Macska indexe: {0}",(int)Animal.cat);
+            Console.WriteLine("Macska indexe: {0}", (int)Animal.wolf);
+            Console.WriteLine("Macska indexe: {0}", (int)Animal.tiger);
+            Console.WriteLine("Macska indexe: {0}", (int)Animal.dog);
+            Console.ReadKey();
+        }
+    }
+}
