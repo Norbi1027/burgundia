b3eb68ff2cc36b0a3d440011d82383a6bb3f56e9
Norbi <75044724+Norbi1027@users.noreply.github.com>
2023-02-06 12:05:29 +0100

    Add files via upload

---
+++ számok bekérése/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace számok_bekérése
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adjon meg egy számot: ");
+            int a = int.Parse(Console.ReadLine());
+            if (a %9 == 0)
+            {
+                Console.WriteLine("Osztható 9-el és 3-al is.");
+            }
+            else if (a %4 == 0) {
+                Console.WriteLine("Osztható 4-el");
+            }
+            else if (a %3 == 0) {
+                Console.WriteLine("Osztható 3al ");
+            }
+            else {
+                Console.WriteLine("Nem osztható 3-al 4-el és 9-el se.");
+                    }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ átváltások/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace átváltások
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add meg a hőmérséklet értékét.");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Add meg mibe szertnéd válatni.(c/f)");
+            string b = Console.ReadLine();
+
+        if (b == "c")
+            {
+                int c =(a - 32) / 9 * 5;
+                Console.WriteLine($"A kapott érték:{c}fok");
+                    }
+        else if (b == "f"){
+                int f = a / 5 * 9 + 32;
+                Console.WriteLine($"A kapott érték:{f}fahrenheit");
+          
+            }
+            Console.ReadKey();
+        }
+    }
+}
