122f36f1240d993bf469a2b00272bddfe186a336
Norbi <75044724+Norbi1027@users.noreply.github.com>
2023-02-09 11:00:16 +0100

    Add files via upload

---
+++ Respirációs kvóciens kiszámítása/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Respirációs_kvóciens_kiszámítása
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Írd be a co2-t: ");
+            double co2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Írd be au o2-t: ");
+            double o2 = Convert.ToDouble(Console.ReadLine());
+
+            double osszeg = co2 / o2;
+            Console.WriteLine(osszeg);
+            if (osszeg == 0.8) {
+                Console.WriteLine("Az érték megfelelő.");
+            
+            }
+            else if (osszeg >0.8)
+            {
+                Console.WriteLine("A szénhidrátbol nyeri az energiát.");
+            }
+            else if (osszeg <0.8)
+            {
+                Console.WriteLine("A zsírbol nyeri az energiát.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ véletlen_szamok_listaja/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace véletlen_szamok_listaja
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int c1 = 0;
+            Console.WriteLine("Első szám: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("második szám: ");
+            int b = int.Parse(Console.ReadLine());
+            Random random1 = new Random();
+            c1 = random1.Next(a,b);
+            for (int j = 0; j <3; j++)
+            {
+                Console.WriteLine(random1.Next(a,b));
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
