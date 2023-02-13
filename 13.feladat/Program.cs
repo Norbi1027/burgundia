483dd9168f02bd387536fb4b1fa2216ef668249d
Norbi <75044724+Norbi1027@users.noreply.github.com>
2023-02-13 13:34:40 +0100

    Add files via upload

---
+++ 13.feladat/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add meg a dinnye átmérőjét");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 14.feladat/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add meg a terület szélességét");
+            Console.WriteLine("Add neg a terület magasságát");
+
+            Console.ReadKey();
+        }
+    }
+}
