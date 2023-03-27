9c96f68155e2e91c87ebde8a696989654397e338
Norbi <75044724+Norbi1027@users.noreply.github.com>
2023-03-27 13:48:48 +0200

    Add files via upload

---
+++ cimletOOP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cimletOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cimletek c1 = new Cimletek(); //Össze kapcsolás: Item & cs
+            Console.WriteLine("Írjon be egy összeget: "); //Adat bekérés
+            int pnz = Convert.ToInt32(Console.ReadLine()); //Érték vissza adás
+            c1.setCimletHely(pnz); //Kiiratom az érték megoldását
+            Console.ReadLine(); //Lezárás
+        }
+    }
+}
---
+++ cimletOOP/cimlet.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cimletOOP
+{
+    internal class Cimletek
+    {
+        //osztályváltorók
+        private int cimlet_hely; //számítások
+        static int[] penz = { 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000 }; //tömb, adatok megadása, tárolása
+
+        //Konstruktor
+        public Cimletek() { }
+
+        //Eredményt átadó konstruktor
+        public Cimletek(int cimlet_hely)
+        {
+            this.cimlet_hely = cimlet_hely;
+        }
+
+        //Eredményeket átadó metódusok
+        public int getCimletHely()
+        {
+            return cimlet_hely;
+        }
+
+        //Számítás
+        public void setCimletHely(int cimlet_hely)
+        {
+            this.cimlet_hely = cimlet_hely;
+            int val = 0; //Adat gyűjtés
+            for (int i = penz.Length - 1; i >= 0; i--) //Számolás ciklusa
+            {
+                while (cimlet_hely >= penz[i])
+                {
+                    cimlet_hely -= penz[i];
+                    val++;
+                }
+            }
+            Console.WriteLine(val); //Kiiratom az értéket
+        }
+    }
+}
