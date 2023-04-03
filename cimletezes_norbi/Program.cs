6a6d76263717abc569c0c3da260e919232f207d4
Norbi <75044724+Norbi1027@users.noreply.github.com>
2023-04-03 13:38:56 +0200

    Add files via upload

---
+++ cimletezes_norbi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CimletekOOP
+{
+    internal class CimletekOOP2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Írjon be egy összeget: ");
+            int osszeg = Convert.ToInt32(Console.ReadLine());
+
+            var Penz = new Penz();
+            var penzek = Penz.getCimleth();
+
+            Console.WriteLine("\n címletek :):");
+            Console.WriteLine(Penz.setCimleth(osszeg));
+
+            Console.ReadKey();
+
+        }
+    }
+}
