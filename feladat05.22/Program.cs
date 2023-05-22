c77e9e16f1b744a752686439be3162bb08de2c50
Norbi <75044724+Norbi1027@users.noreply.github.com>
2023-05-22 11:49:20 +0200

    Add files via upload

---
+++ feladat05.22/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+
+    static void Main()
+    {
+        bool vege = false;
+        while (vege != true)
+        {
+
+
+            Console.WriteLine("Válassz műveletet:");
+            Console.WriteLine("1 - Létrehozás és Írás a fájlba");
+            Console.WriteLine("2 - Olvasás a fájlból");
+            Console.WriteLine("3 - Fájl törlése");
+            Console.WriteLine("4 - Meglévő Fájlba írás");
+            Console.WriteLine("5 - Kilépés");
+
+            int valasztas = Convert.ToInt32(Console.ReadLine());
+
+            switch (valasztas)
+            {
+                case 1:
+                    Iras();
+                    break;
+                case 2:
+                    Olvasas();
+                    break;
+                case 3:
+                    Torles();
+                    break;
+                case 4:
+                    Hozzaad();
+                    break;
+                case 5:
+                    vege = true;
+                    break;
+                default:
+                    Console.WriteLine("Érvénytelen választás!");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+    static void Iras()
+    {
+        Console.WriteLine("Add meg a fájlnevet:");
+        string fajlnev = Console.ReadLine();
+
+        Console.WriteLine("Írj valamit a fájlba:");
+        string tartalom = Console.ReadLine();
+
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(fajlnev))
+            {
+                writer.WriteLine(tartalom);
+            }
+
+            Console.WriteLine("Sikeresen írva a fájlba!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Hiba történt az írás során: " + ex.Message);
+        }
+    }
+
+    static void Olvasas()
+    {
+        Console.WriteLine("Add meg a fájlnevet:");
+        string fajlnev = Console.ReadLine();
+
+        try
+        {
+            using (StreamReader reader = new StreamReader(fajlnev))
+            {
+                string tartalom = reader.ReadToEnd();
+                Console.WriteLine("A fájl tartalma:");
+                Console.WriteLine(tartalom);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Hiba történt az olvasás során: " + ex.Message);
+        }
+    }
+    static void Torles()
+    {
+        Console.WriteLine("Add meg a fájlnevet:");
+        string fajlnev = Console.ReadLine();
+
+        File.Delete(fajlnev);
+        Console.WriteLine("A file sikeresen törölve.");
+    }
+    static void Hozzaad()
+    {
+        Console.WriteLine("Add meg a fájlnevet:");
+        string fajlnev = Console.ReadLine();
+
+        Console.WriteLine("Mit szertnél hozzá adni?");
+        string hozzaad = Console.ReadLine();
+
+        using (StreamWriter sw = File.AppendText(fajlnev))
+        {
+            sw.WriteLine(hozzaad);
+        }
+
+    }
+}
